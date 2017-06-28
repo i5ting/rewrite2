f0c7f8ffc58a56caa3580a944c433ac3c705cdf7
shilong.sang <i5ting@126.com>
2017-06-28 14:45:19 +0800

    refact, please ignore

--- rewrite2.js
+++ rewrite2.js
@@ -1,3 +1,10 @@
+const rewrite2 = require(".");
+const extend = require("xtend");
 
+module.exports = function (req, res, config) {
+    let _config = extend({
+        jsonp: true
+    }, config)
 
-module.exports = 
+    rewrite2(req, res, _config);
+}
